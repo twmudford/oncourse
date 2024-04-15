ac4b2f2657c1f5da061549455c9d6d82a22033ff
Tim Mudford <tim.mudford@serato.com>
2024-04-15 16:59:24 +1200

    Test GPS API

--- index.js
+++ index.js
@@ -1839,22 +1839,19 @@ function get_course() {
 }
 
 
-function mytest() {
-    const x = document.getElementById("demo");
 
-    function getLocation() {
-        console.log(navigator.geolocation)
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
+const x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
-
-    function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude +
-            "<br>Longitude: " + position.coords.longitude;
-    }
-
 }
-mytest()
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
